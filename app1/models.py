260749c905cc2d620b928b2896a33fbdf33778ea
AMOLDSHINDE <shindeas1112@gmail.com>
2024-02-21 11:17:03 +0530

    add

---
+++ app1/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length =100)
+    age = models.IntegerField()
+    mark = models.IntegerField()
+    school = models.CharField(max_length= 100)
+
+    class Meta:
+        db_table = 'student'
+
+    def __str__(self):
+        return self.name 
---
+++ app1/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from .models import Student
+from .serializar import StudentSerializer
+# Create your views here.
+
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, DestroyModelMixin, UpdateModelMixin, RetrieveModelMixin
+
+class List_all(GenericAPIView, ListModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+class Create_single(GenericAPIView, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+class Updata(GenericAPIView, UpdateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+class Retrive_single(GenericAPIView, RetrieveModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+class Destroy_single(GenericAPIView, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
